db1bb1652f592f7196a3c140ab27d7d654f5894d
Kevin Boyd <63878894+kevinboyd76@users.noreply.github.com>
2026-02-10 16:19:01 -0600

    Create submit_fastq2ezbakr.sh

---
+++ submit_fastq2ezbakr.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#SBATCH -A sansam-lab
+#SBATCH -p serial
+#SBATCH -t 08:00:00
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=1
+#SBATCH -J fastq2EZbakR.master
+#SBATCH -o logs/slurm-master-%j.out
+#SBATCH -e logs/slurm-master-%j.err
+
+set -euo pipefail
+mkdir -p logs
+
+module load python/3.10
+
+snakemake \
+  -j 20 \
+  --use-envmodules \
+  --rerun-incomplete \
+  --keep-going \
+  --latency-wait 600 \
+  --printshellcmds \
+  --cluster-config config/cluster_config.yaml \
+  --cluster "sbatch -A {cluster.account} -p {cluster.partition} -t {cluster.time} --mem {cluster.mem} --cpus-per-task {cluster.cpus-per-task} -J {cluster.name} -o {cluster.output} -e {cluster.error}"
